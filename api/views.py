e5765b095eb5fb7bad3ac7b3aacd5e4f11d98271
Sasha Mikhailov <sasha@sashamikhailov.ru>
2021-05-17 06:32:54 +0300

    add views

--- api/views.py
+++ api/views.py
@@ -1,1 +1,47 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth import get_user_model
+from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
+
+from .models import Post
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, PostSerializer
+
+User = get_user_model()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    ]
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs["post_id"])
+
+    def get_queryset(self):
+        post = self.get_post()
+        queryset = post.comments
+
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            queryset = queryset.all()
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
